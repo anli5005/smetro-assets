17dbec2b1647830c6e761fad27a9064f1423e728
Anthony Li <li.anthony917@gmail.com>
2018-06-05 21:12:40 -0400

    Redesign map

---
+++ stations-scroll.lua
@@ -0,0 +1,138 @@
+{
+    {
+        name = "a26-glass-desert",
+        label = "Glass Desert",
+        x = 18,
+        y = 6,
+        w = 2,
+        h = 1
+    },
+    {
+        name = "a23-forest",
+        label = "Forest",
+        x = 18,
+        y = 10,
+        w = 2,
+        h = 1
+    },
+    {
+        name = "a21-meander-bridge",
+        label = "Meander Bridge",
+        x = 18,
+        y = 12,
+        w = 2,
+        h = 1
+    },
+    {
+        name = "a15-sugar-cane",
+        label = "Sugar Cane",
+        x = 18,
+        y = 14,
+        w = 2,
+        h = 1
+    },
+    {
+        name = "a14-acacia-jct",
+        label = "Acacia Junction",
+        x = 18,
+        y = 16,
+        w = 2,
+        h = 1
+    },
+    {
+        name = "1363-glass-beach",
+        label = "Glass Beach",
+        x = 9,
+        y = 18,
+        w = 1,
+        h = 2
+    },
+    {
+        name = "a1362-acacia-pass",
+        label = "Acacia Pass",
+        x = 16,
+        y = 18,
+        w = 1,
+        h = 2
+    },
+    {
+        name = "a134-quarry",
+        label = "Quarry",
+        x = 20,
+        y = 18,
+        w = 1,
+        h = 2
+    },
+    {
+        name = "a1338-far-quarry",
+        label = "Far Quarry",
+        x = 27,
+        y = 18,
+        w = 1,
+        h = 2
+    },
+    {
+        name = "a1335-east-hills",
+        label = "East Hills",
+        x = 34,
+        y = 18,
+        w = 1,
+        h = 2
+    },
+    {
+        name = "a1331-slime-island",
+        label = "Slime Island",
+        x = 40,
+        y = 18,
+        w = 1,
+        h = 2
+    },
+    {
+        name = "a12-smeltery",
+        label = "Smeltery",
+        x = 18,
+        y = 23,
+        w = 2,
+        h = 1
+    },
+    {
+        name = "a113-spawn-point",
+        label = "Spawn Point",
+        x = 21,
+        y = 25,
+        w = 2,
+        h = 1
+    },
+    {
+        name = "a10-crystal-tower",
+        label = "Crystal Tower",
+        x = 18,
+        y = 27,
+        w = 2,
+        h = 1
+    },
+    {
+        name = "a096-sand-lake",
+        label = "Sand Lake"
+        x = 18,
+        y = 29,
+        w = 2,
+        h = 1
+    },
+    {
+        name = "a094-ocean",
+        label = "Ocean",
+        x = 18,
+        y = 31,
+        w = 2,
+        h = 1
+    },
+    {
+        name = "a088-mtn-archway",
+        label = "Mountain Archway",
+        x = 12,
+        y = 32,
+        w = 1,
+        h = 2
+    }
+}
